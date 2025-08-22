dd00d1e4a40330989f334e616114f5c6ef89e221
Charles Lee <2535849195@qq.com>
2025-08-22 17:34:05 +0800

    init: init commit

---
+++ apps/web/vite.config.ts
@@ -0,0 +1,9 @@
+import tailwindcss from '@tailwindcss/vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import { defineConfig } from 'vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [vue(), vueJsx(), tailwindcss()],
+})
